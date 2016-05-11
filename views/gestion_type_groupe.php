ad604dd4108a74f2751b865bf1e64e44507fce51
Sebastien <s.toursel@gmail.com>
2016-05-11 15:38:27 +0200

    V1 de types groupes

--- views/gestion_type_groupe.php
+++ views/gestion_type_groupe.php
@@ -7,17 +7,26 @@
     <div class="col-md-12 align-center">
         <label class="trigger btn btn-success" for="popup__1" onclick="changeActionForm('add_type_groupe.php');">Ajouter</label>
     </div>
-    <?php TabTypeGrp();AddButBeg();?>
-    <form action="mod_type_groupe.php" method="post" name="form_pop" id="form_pop">
-        <div class="col-md-6">
-            <input type="hidden" name="id" id="id_mod"/>
-            <input type="text" name="libelle" placeholder="Nom du type de groupe *" class="input_cus_add" id="libelle_mod"/>
-        </div>
-        <div class="col-md-6">
-            <input type="number" name="points" placeholder="Nombre de points associé*" class="input_cus_add" id="point_mod"/>
-        </div>
-    <button type="submit" class="but_cus">Valider</button>
-    </form> 
+    <?php TabTypeGrp();
+
+    AddButBeg();?>
+    </table>
+    <input type="submit" value="Supprimer" class="btn btn-danger">
+    </form>
+
+    <button id="boutMod"class="btn btn-success" data-toggle="collapse" data-target="#collapse">Modifier</button>
+
+<div id="collapse" class="col-md-12 collapse">    
+    <h1>Modifications</h1>
+    <h3>Etape 1° - Veuillez saisir le nom du type de groupe à modifier</h3>
+    <form action="mod_type_groupe.php" method="POST">
+        <input type="texte" name="groupName">
+    <h3>Etape 2° - Veuillez saisir le nombre de points que vous souhaitez attribuer</h3>
+        <input type="texte" name="newNumber" value=""><br>
+        <br>
+        <input type="submit"class="btn btn-success">
+    </form>
+</div>
     <?php AddButEnd(); ?>
 </div>
 </div>
